955b395d68d0200765b0178546bfe52250ea6c5a
Safe <amir33333jxb@gmail.com>
2026-07-17 02:40:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,10 +29,31 @@ sed -i "s|VpnSafee_bot|$BOT|g" "$TEMPLATE_PATH/index.html"
 sed -i "s|SafeVpn13|$CHANNEL|g" "$TEMPLATE_PATH/index.html"
 sed -i "s|SafeVpn5|$SUPPORT|g" "$TEMPLATE_PATH/index.html"
 
+# ایجاد فایل کانفیگ برای متغیرهای مورد نیاز
+cat > "$TEMPLATE_PATH/config.json" <<EOF
+{
+    "template": "Safe-Sub",
+    "variables": {
+        "subId": "user.id",
+        "subUrl": "user.sub_url",
+        "expire": "user.expire_time",
+        "lastOnline": "user.last_online",
+        "totalByte": "user.total_bytes",
+        "downloadByte": "user.download_bytes",
+        "uploadByte": "user.upload_bytes",
+        "enabled": "user.enabled",
+        "subTitle": "user.sub_title",
+        "links": "user.links",
+        "emails": "user.emails"
+    }
+}
+EOF
+
 cp "$TEMPLATE_PATH/index.html" "$TEMPLATE_PATH/sub.html"
 
 chmod 644 "$TEMPLATE_PATH/index.html"
 chmod 644 "$TEMPLATE_PATH/sub.html"
+chmod 644 "$TEMPLATE_PATH/config.json"
 
 echo
 echo "=================================="
@@ -40,4 +61,8 @@ echo " Installation completed!"
 echo
 echo " Template Path:"
 echo " $TEMPLATE_PATH"
+echo
+echo " Make sure to:"
+echo " 1. Upload the new template.html to your repository"
+echo " 2. Configure 3x-ui to use 'Safe-Sub' template"
 echo "=================================="
